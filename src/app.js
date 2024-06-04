77e9f5346df1f0dc37b94a50ab0158ee64d0069a
Jere <jeeretoledo@gmail.com>
2024-06-04 19:16:03 -0300

    corrigiendo app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-// Middlewares
 app.use(express.json());
 
-// Rutas
-const productsRouter = require('./routers/products_router');
-const cartsRouter = require('./routers/carts_router');
+const productsRouter = require("./routers/products_router");
+const cartsRouter = require("./routers/carts_router");
 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
